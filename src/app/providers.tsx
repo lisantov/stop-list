5b191ea0c2b56338c46eab4a3058ad9999396646
lisantov <lisantov@yandex.ru>
2026-09-13 15:32:05 +0700

    Init

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/query-client";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => getQueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
